4e13c6e5a63a67abd3c4367700a8a7d535884f13
Eric C. Black <eric@bitacoustic.net>
2015-06-25 14:10:35 -0700

    Comment modifications

--- app/src/main/java/com/csc413/team5/fud5/RestaurantList.java
+++ app/src/main/java/com/csc413/team5/fud5/RestaurantList.java
@@ -58,47 +58,18 @@ public class RestaurantList implements RestaurantListInterface {
      Getters
      *******/
 
-    /**
-     * Returns the Restaurant at specified index.
-     *
-     * @param index get restaurant by index
-     * @return Restaurant at specified index, or null if index is invalid
-     */
-
     public Restaurant getRestaurant(int index) {
         return null; // TODO
     }
 
-    /**
-     * Returns the first Restaurant matching specified ID String.
-     *
-     * @param id database identifier String, e.g. Yelp ID
-     * @return Restaurant object, or null if restaurant was not found
-     */
     public Restaurant getRestaurant(String id) {
         return null; // TODO
     }
 
-    /**
-     * @return number of entries in list
-     */
     public int getSize() {
         return restaurants.size();
     }
 
-    /**
-     * Returns a MapBounds object holding suggested bounds of a map which
-     * displays the restaurants in the list.
-     * <p/>
-     * Example:
-     * <p/>
-     * <pre>
-     *     double longitude = myRestaurantList.getMapBounds().getLongitude();
-     * </pre>
-     *
-     * @return a MapBounds object
-     * @see MapBounds
-     */
     public MapBounds getMapBounds() {
         return bounds;
     }
@@ -107,13 +78,6 @@ public class RestaurantList implements RestaurantListInterface {
      Setters
      *******/
 
-    /**
-     * Sets contents of this restaurant list based on an encoded list of restaurants
-     * from a restaurant API query, e.g. JSON-encoded String from Yelp API query
-     *
-     * @param encodedInput  encoded String with a list of restaurants
-     * @throws IOException if object read unsuccessful
-     */
     public void setRestaurantList(String encodedInput) throws IOException {
         // clear existing list data if there is any
         restaurants.clear();
@@ -188,66 +152,30 @@ public class RestaurantList implements RestaurantListInterface {
      Other methods
      *************/
 
-    /**
-     * Remove the Restaurant at the specified index.
-     *
-     * @param index
-     * @return the Restaurant removed if successful, otherwise null
-     */
     public Restaurant removeRestaurant(int index) {
         return restaurants.remove(index);
     }
 
-    /**
-     * Removes a restaurant from the list based on database identifier, e.g. Yelp ID
-     *
-     * @param id database identifier
-     * @return the Restaurant removed if successful, otherwise null
-     */
     public Restaurant removeRestaurant(String id) {
         return null; // TODO
     }
 
-    /**
-     * Recently YellowListed items are demoted, older YellowListed items are
-     * promoted.
-     */
     public void applyYellowList() {
         // TODO
     }
 
-    /**
-     * Removes RedListed restaurants from list.
-     */
     public void applyRedList() {
         // TODO
     }
 
-    /**
-     * Sorts restaurants exclusively by distance from search location.
-     */
     public void sortByDistance() {
         // TODO
     }
 
-    /**
-     * Demotes the specified Restaurant the specified number of places in the list,
-     * or to the end of the list, whichever is closer to its current position.
-     *
-     * @param id        database identifier String
-     * @param numPlaces an integer number of indices
-     */
     public void demote(String id, int numPlaces) {
         // TODO
     }
 
-    /**
-     * Promotes the specified Restaurant the specified number of places in the list,
-     * or to the beginning of the list, whichever is closer to its current position.
-     *
-     * @param id        database identifier String
-     * @param numPlaces an integer number of indices
-     */
     public void promote(String id, int numPlaces) {
         // TODO
     }
