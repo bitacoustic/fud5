0e867c53ff5f9533c53d0bb1065279b4c7e1e343
Whogie <whogie@gmail.com>
2015-06-26 00:56:04 -0700

    Added a sample button for reference for the time being.

--- app/src/main/java/com/csc413/team5/fud5/MainActivity.java
+++ app/src/main/java/com/csc413/team5/fud5/MainActivity.java
@@ -2,11 +2,25 @@ package com.csc413.team5.fud5;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
+
+    public void btnSampleButton(View v){
+        //Assign the button to an object (Not actually used in this example)
+        Button sampleButton = (Button) findViewById(R.id.btnSampleButton);
+        //Assign the TextView (the "Hello World") to an object
+        TextView helloWorld = (TextView) findViewById(R.id.txtHelloWorld);
+
+        //Change the text in the Text View
+        helloWorld.setText(R.string.button_pressed); //String is stored in strings.xml
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
