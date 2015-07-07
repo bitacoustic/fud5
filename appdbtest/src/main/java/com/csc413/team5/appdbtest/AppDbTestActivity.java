4f2972215c22deebfedb7bbde2069fcbc6b7d792
niculistana <niculistana@gmail.com>
2015-07-06 20:34:29 -0700

    Deleted all test code from main /app folder and created new module /appdbtest for all the test code

--- appdbtest/src/main/java/com/csc413/team5/appdbtest/AppDbTestActivity.java
+++ appdbtest/src/main/java/com/csc413/team5/appdbtest/AppDbTestActivity.java
@@ -1,53 +1,38 @@
-package com.csc413.team5.fud5;
+package com.csc413.team5.appdbtest;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 import com.csc413.team5.appdb.dbHelper;
-import com.csc413.team5.restaurantapiwrapper.Restaurant;
 
+public class AppDbTestActivity extends AppCompatActivity {
 
-public class GreenListActivity extends AppCompatActivity {
-
-    dbHelper db;
+    private dbHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_green_list);
+        setContentView(R.layout.activity_app_db_test);
 
-        /* display DB Information */
         db = new dbHelper(this, null, null, 1);
+        db.getWritableDatabase();
+
         TextView dbTitle = (TextView) findViewById(R.id.dbTitle);
         TextView dbPath = (TextView) findViewById(R.id.dbPath);
-        TextView dbTable = (TextView) findViewById(R.id.dbTableTitle);
 
         dbTitle.append(db.getDbName());
         dbPath.append(db.getDbPath(this));
-        dbTable.append(db.getDbTableName(1));
-
-        /* display DB contents */
-        TextView restaurantIDEntries = (TextView) findViewById(R.id.green_restaurant_id_text);
-
-        if (db.isTableExist(1) && !db.isTableEmpty(1)) { // if db exists and is not empty
-            for (int i = 0; i < db.getRestaurantNamesFromList(1).size(); i++) {
-                restaurantIDEntries.append("\n" + db.getRestaurantNamesFromList(1).get(i));
-            }
-        }
-
-        restaurantIDEntries.append("\n" + "---TABLE INFO---");
-        restaurantIDEntries.append("\n" + "Exists: " + db.isTableExist(1));
-        restaurantIDEntries.append("\n" + "Empty: " + db.isTableEmpty(1));
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_green_list, menu);
+        getMenuInflater().inflate(R.menu.menu_app_db_test, menu);
         return true;
     }
 
@@ -64,5 +49,25 @@ public class GreenListActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void createRecord(View v) {
+        dbColorDialogFragment createDialog = dbColorDialogFragment.newInstance(1);
+        createDialog.show(getFragmentManager(), "createDialog");
+    }
+
+    public void readRecord(View v) {
+        dbColorDialogFragment readDialog = dbColorDialogFragment.newInstance(2);
+        readDialog.show(getFragmentManager(), "readDialog");
+    }
+
+    public void updateRecord(View v) {
+        dbColorDialogFragment updateDialog = dbColorDialogFragment.newInstance(3);
+        updateDialog.show(getFragmentManager(), "updateDialog");
+    }
+
+    public void deleteRecord(View v) {
+        dbColorDialogFragment deleteDialog = dbColorDialogFragment.newInstance(4);
+        deleteDialog.show(getFragmentManager(), "deleteDialog");
     }
 }
