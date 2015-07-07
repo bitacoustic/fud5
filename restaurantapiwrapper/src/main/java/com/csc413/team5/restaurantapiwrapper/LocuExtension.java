bda8d066a7d89f57feaeca5aa1fdb648089170eb
Eric C. Black <eric@bitacoustic.net>
2015-07-06 17:38:13 -0700

    Restored modules restaurantapiwrapper, restauranttest to current working versions

---
+++ restaurantapiwrapper/src/main/java/com/csc413/team5/restaurantapiwrapper/LocuExtension.java
@@ -0,0 +1,168 @@
+package com.csc413.team5.restaurantapiwrapper;
+
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.scribe.model.OAuthRequest;
+import org.scribe.model.Response;
+import org.scribe.model.Verb;
+
+/**
+ * Locu extension for {@link RestaurantApiClient}; WARNING: not yet fully implemented -- I've
+ * uploaded this so the module 'RestaurantTest' works.
+ * <p>
+ * Created on 7/2/2015.
+ *
+ * @author Eric C. Black
+ */
+public class LocuExtension {
+    private static final String TAG = "LocuExtension";
+    private final LocuApiKey key;
+
+
+    public LocuExtension(LocuApiKey key) {
+        this.key = key;
+    }
+
+    /**
+     * Attempts to match the specified {@link Restaurant} to a venue in Locu's database, extracting
+     * the following information if it is available:
+     * <ul>
+     *     <li>Open hours
+     *     <li>Restaurant menu
+     * </ul>
+     * <p>The Restaurant will be updated in place to reflect the additional information.
+     * @param r a Restaurant object
+     * @return the Restaurant
+     */
+    public Restaurant update(Restaurant r) {
+        String id = getLocuId(r);
+        if (id.compareTo("") == 0) // match not found
+            return r;              // don't update Restaurant
+
+        updateFromMatchedLocuId(r, id);
+
+        return r;
+    }
+
+    /**
+     * Attempts to get the Locu ID for the given Restaurant, using the Restaurant's geolocation,
+     * name, address, and postal code. If no match was found, returns an empty String ("").
+     * @param r a Restaurant object
+     * @return a Locu ID if match was found or if it is already stored in the Restaurant,
+     *         "" if ID information is otherwise unavailable
+     */
+    public String getLocuId(Restaurant r) {
+
+        String rName = r.name;
+        int indexNameFirstSpace = rName.indexOf(' ');
+        if (indexNameFirstSpace > 0)
+            rName = rName.substring(0, indexNameFirstSpace);
+
+        String rAddr = r.address.getAddressLine(0).replaceAll(" ", "%20");
+
+        String url = "http://api.locu.com/v1_0/venue/search/?"
+                + "location=" + Double.toString(r.addressMapable.getLatitude())
+                + "%2C" + Double.toString(r.addressMapable.getLongitude())
+                + "&name=" + rName + "&address=" + rAddr
+                + "&postal_code=" + r.address.getPostalCode()
+                + "&api_key=" + key.getKey();
+
+        OAuthRequest request = new OAuthRequest(Verb.GET, url);
+
+        Log.i(TAG, "Sending Locu request: " + request.getUrl());
+        Response response = request.send();
+        Log.i(TAG, "Received response: " + response.getMessage());
+        Log.i(TAG, "Response: " + response.getBody());
+
+        // if GET response was not OK
+        if (response.getMessage().compareTo("OK") != 0)
+            return "";
+
+        JSONObject in = null;
+        try {
+            in = new JSONObject(response.getBody());
+            JSONArray inObjects = in.getJSONArray("objects");
+            if (inObjects.length() > 0) {
+                JSONObject inObjectsMatch = inObjects.getJSONObject(0);
+                if (inObjectsMatch.has("id"))
+                    return inObjectsMatch.getString("id");
+                else
+                    return ""; // id not present, return""
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();  // if can't read response, match not possible
+            return "";
+        }
+
+        return ""; // default: can't get Locu ID for match
+    }
+
+    /**
+     * Parse information about a Locu venue and add it to the Restaurant. If the ID is not
+     * found in the Locu database the Restaurant will not be updated.
+     * @param r   {@link Restaurant}
+     * @param id  a Locu venue ID
+     */
+    public void updateFromMatchedLocuId(Restaurant r, String id) {
+        String url = "http://api.locu.com/v1_0/venue/" + id + "/?api_key=" + key.getKey();
+        OAuthRequest request = new OAuthRequest(Verb.GET, url);
+        Log.i(TAG, "Sending Locu request: " + request.getUrl());
+        Response response = request.send();
+        Log.i(TAG, "Received response: " + response.getMessage());
+        if (response.getMessage().compareTo("OK") != 0)
+            return; // query unsuccessful; don't update Restaurant
+
+        JSONObject in;
+        try {
+            in = new JSONObject(response.getBody());
+            if (in != null) {
+                JSONArray inNames = in.names();
+                for (int i = 0; i < inNames.length(); i++) {
+                    switch (inNames.getString(i)) {
+                        case "not_found":
+                            JSONArray inNotFound = in.getJSONArray("not_found");
+                            if (inNotFound.length() > 0)
+                                return; // id not found; don't update Restaurant
+                            break;
+                        case "objects":
+                            JSONArray inObjects = in.getJSONArray("objects");
+                            JSONObject inObjectsVenue = inObjects.getJSONObject(0);
+                            updateFromMatchedLocuIdHelper(inObjectsVenue, r);
+                        default:
+                            break;
+                    } // end switch
+                } // end if
+            } // end try
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return; // JSON exception; don't update Restaurant
+        }
+
+        // default: JSONObject is null; don't update Restaurant
+    } // end updateFromMatchedLocuId()
+
+    /**
+     * Helper for updateFromMatchedLocuId()
+     * @param in single object in "objects" field of Locu venue detail as JSONObject
+     * @param r  a Restaurant
+     */
+    private void updateFromMatchedLocuIdHelper(JSONObject in, Restaurant r) {
+        if (in.has("menus")) {
+            r.locuMenus = new LMenus();
+            // TODO finish this
+        }
+        if (in.has("open_hours")) {
+            try {
+                JSONObject inHours = in.getJSONObject("open_hours");
+                r.hours.clear();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
---
+++ restauranttest/src/main/java/com/csc413/team5/restauranttest/RestaurantTestActivity.java
@@ -0,0 +1,164 @@
+package com.csc413.team5.restauranttest;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.csc413.team5.restaurantapiwrapper.LocuApiKey;
+import com.csc413.team5.restaurantapiwrapper.LocuExtension;
+import com.csc413.team5.restaurantapiwrapper.MapBounds;
+import com.csc413.team5.restaurantapiwrapper.Restaurant;
+import com.csc413.team5.restaurantapiwrapper.RestaurantApiClient;
+import com.csc413.team5.restaurantapiwrapper.RestaurantList;
+import com.csc413.team5.restaurantapiwrapper.YelpApiKey;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+
+
+public class RestaurantTestActivity extends ActionBarActivity {
+    private TextView callOutput;
+    private Restaurant someRestaurant;
+    private RestaurantList someRestaurantList;
+    private String testString;
+    private MapBounds testMapBounds;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("Yelp API Test");
+
+        callOutput = (TextView) findViewById(R.id.api_out);
+        callOutput.setText("Initiating call ... \n\n");
+
+        YelpAsyncTask task = new YelpAsyncTask();
+        task.execute();
+    }
+
+    private class YelpAsyncTask extends AsyncTask<String, String, String> {
+        private String response;
+
+        @Override
+        protected String doInBackground(String... params)  {
+            // Construct a YelpApiKey from Resource strings
+            String consumerKey = getApplicationContext().getResources()
+                    .getString(R.string.yelp_consumer_key);;
+            String consumerSecret = getApplicationContext().getResources()
+                    .getString(R.string.yelp_consumer_secret);;
+            String tokenKey = getApplicationContext().getResources()
+                    .getString(R.string.yelp_token);;
+            String tokenSecret = getApplicationContext().getResources()
+                    .getString(R.string.yelp_token_secret);;
+            YelpApiKey yelpKey = new YelpApiKey(consumerKey, consumerSecret, tokenKey, tokenSecret);
+
+            // search for an individual business by Yelp ID
+            // change id field to test different businesses
+
+            RestaurantApiClient rClient = new RestaurantApiClient.Builder(yelpKey)
+                    .id("squat-and-gobble-san-francisco-2").build();
+            try {
+                someRestaurant = rClient.getRestaurantByYelpID();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+
+            /*
+            FactualApiKey factualKey = new FactualApiKey("", "");
+            FactualExtension factual = new FactualExtension(factualKey);
+            String fResult = factual.match(someRestaurant);
+
+            callOutput.append("\n\n" + fResult);
+            */
+
+            // WARNING: Locu functionality isn't yet fully implemented
+            LocuApiKey locuKey = new LocuApiKey(getApplicationContext().getResources()
+                    .getString(R.string.locu_key));
+            LocuExtension locu = new LocuExtension(locuKey);
+            testString = locu.getLocuId(someRestaurant);
+
+            //
+
+            try {
+                rClient = new RestaurantApiClient.Builder(yelpKey).location("Hayward, CA")
+                        //.categoryFilter("foodtrucks,restaurants")
+                        .term("seafood")
+                        //.radiusFilter(15000)
+                        .build();
+                someRestaurantList = rClient.getRestaurantList();
+                testMapBounds = someRestaurantList.getMapBounds();
+            //} catch (ParameterRangeException e) {
+            //    e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return "done";
+
+        }
+
+        protected void onPostExecute(String result) {
+            callOutput.append("\nResult of Yelp ID request:\n");
+            if (someRestaurant == null)
+                callOutput.append("null Restaurant()\n");
+            else
+                callOutput.append(someRestaurant.toString());
+
+            callOutput.append("\n\nAttempt at Locu match:\n");
+            if (testString.compareTo("") == 0)
+                callOutput.append("No match found.");
+            else
+                callOutput.append("Matched Locu ID: " + testString);
+
+            callOutput.append("\n\nResult of search request:\n");
+            if (someRestaurantList == null)
+                callOutput.append("null RestaurantList()\n");
+            else {
+                callOutput.append("RestaurantList size: " + someRestaurantList.size());
+                callOutput.append("\nMap bounds after query: " + testMapBounds);
+                int rListMid = someRestaurantList.size() / 2;
+                someRestaurantList.remove(rListMid);
+                callOutput.append("\nRemoved 1 record. RestaurantList size: "
+                        + someRestaurantList.size());
+                someRestaurantList.add(someRestaurant);
+                callOutput.append("\nAdded 1 record. RestaurantList size: "
+                        + someRestaurantList.size());
+                callOutput.append("\n\nThe contents of the" +
+                        "RestaurantList is now:");
+                callOutput.append(someRestaurantList.toString());
+            }
+        }
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
