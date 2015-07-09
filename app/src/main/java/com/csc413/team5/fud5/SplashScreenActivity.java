51815cb03dfb67601c114d2187e86de7a20414e1
Alex Brown <abrown@mail.sfsu.edu>
2015-07-08 22:22:59 -0700

    Splash screen compensates for load time. Program launches if user chooses no for Location Services.

--- app/src/main/java/com/csc413/team5/fud5/SplashScreenActivity.java
+++ app/src/main/java/com/csc413/team5/fud5/SplashScreenActivity.java
@@ -1,7 +1,9 @@
 package com.csc413.team5.fud5;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -10,6 +12,7 @@ import android.view.Window;
 import static android.view.WindowManager.*;
 
 public class SplashScreenActivity extends Activity implements AskToUseLocationFragment.NoticeDialogListener  {
+    private long startLoadTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,17 +25,25 @@ public class SplashScreenActivity extends Activity implements AskToUseLocationFr
         this.getWindow().setFlags(LayoutParams.FLAG_FULLSCREEN, LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_splash_screen);
+
+        startLoadTime = System.currentTimeMillis();
     }
 
     @Override
     protected void onStart() {
         // tell the Android OS to open the dialog after 3 seconds (= 3000 milliseconds)
+        long splashScreenTime = 3000; //Magic numbers get their own variable
+        long waitTime =  ((startLoadTime + splashScreenTime) - System.currentTimeMillis());
+        if (waitTime < 0)
+            waitTime = 0;
+
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             public void run() {
                 askToUseLocation();
             }
-        }, 3000);
+        }, waitTime);
+
         super.onStart();
     }
 
@@ -46,13 +57,18 @@ public class SplashScreenActivity extends Activity implements AskToUseLocationFr
     @Override
     public void onDialogPositiveClick(DialogFragment dialog) {
         // TODO save user choice and proceed to the next screen
-        finish();   // removes this activity from the back stack
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+        openMainActivity();
     }
 
     @Override
     public void onDialogNegativeClick(DialogFragment dialog) {
-        // TODO Exit app
+        //It'll prompt them the next time they run the app until they say yes
+        openMainActivity();
+    }
+
+    private void openMainActivity(){
+        finish();   // removes this activity from the back stack
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
