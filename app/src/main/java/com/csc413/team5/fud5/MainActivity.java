8a37a040bd4ef81efe2ed10d5347874b1a34daf6
Eric C. Black <eric@bitacoustic.net>
2015-07-08 19:27:08 -0700

    Splash screen demo with prompt to use device location

--- app/src/main/java/com/csc413/team5/fud5/MainActivity.java
+++ app/src/main/java/com/csc413/team5/fud5/MainActivity.java
@@ -1,26 +1,18 @@
 package com.csc413.team5.fud5;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.DialogFragment;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements AskToUseLocationFragment.NoticeDialogListener {
 
-    public void btnSampleButton(View v){
-        //Assign the button to an object (Not actually used in this example)
-        Button sampleButton = (Button) findViewById(R.id.btnSampleButton);
-        //Assign the TextView (the "Hello World") to an object
-        TextView helloWorld = (TextView) findViewById(R.id.txtHelloWorld);
-
-        //Change the text in the Text View
-        helloWorld.setText(R.string.button_pressed); //String is stored in strings.xml
-    }
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +20,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    protected void onStart() {
+        // tell the Android OS to open the dialog after 3 seconds (= 3000 milliseconds)
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                askToUseLocation();
+            }
+        }, 3000);
+
+        super.onStart();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -49,5 +53,24 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    // Dialog AskToUseLocationFragment
+
+    public void askToUseLocation() {
+        DialogFragment dialogAskToUseLocation = new AskToUseLocationFragment();
+        dialogAskToUseLocation.show(getFragmentManager(), "askToUseLocation");
+    }
+
+    @Override
+    public void onDialogPositiveClick(DialogFragment dialog) {
+        Log.i(TAG, "User agreed to use location services.");
+        // TODO save user choice and proceed to the next screen
+    }
+
+    @Override
+    public void onDialogNegativeClick(DialogFragment dialog) {
+        Log.i(TAG, "User did not agree to use location services.");
+        // TODO save user choice and proceed to the next screen
     }
 }
