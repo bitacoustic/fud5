f9740a4ce2e5ab0f74f14e492560ec4e704b4f3b
niculistana <niculistana@gmail.com>
2015-07-08 20:56:25 -0700

    Added a splash screen that uses askToUseLocation. Modified manifest file to make splash screen the main app. Deleted appInfoInterface.

---
+++ app/src/main/java/com/csc413/team5/fud5/SplashScreenActivity.java
@@ -0,0 +1,54 @@
+package com.csc413.team5.fud5;
+
+import android.app.Activity;
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+import static android.view.WindowManager.*;
+
+public class SplashScreenActivity extends Activity implements AskToUseLocationFragment.NoticeDialogListener  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //Remove notification bar
+        this.getWindow().setFlags(LayoutParams.FLAG_FULLSCREEN, LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash_screen);
+    }
+
+    @Override
+    protected void onStart() {
+        // tell the Android OS to open the dialog after 3 seconds (= 3000 milliseconds)
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                askToUseLocation();
+            }
+        }, 3000);
+        super.onStart();
+    }
+
+
+    // Dialog AskToUseLocationFragment
+    public void askToUseLocation() {
+        DialogFragment dialogAskToUseLocation = new AskToUseLocationFragment();
+        dialogAskToUseLocation.show(getFragmentManager(), "askToUseLocation");
+    }
+
+    @Override
+    public void onDialogPositiveClick(DialogFragment dialog) {
+        // TODO: Show Main Activity
+    }
+
+    @Override
+    public void onDialogNegativeClick(DialogFragment dialog) {
+        // TODO Exit app
+    }
+}
