1d4bc4db233b2bc228d6287fedf77b7f13811d96
niculistana <niculistana@gmail.com>
2015-07-12 21:19:05 -0700

    Implemented user preferences front-end boilerplate.

--- app/src/main/java/com/csc413/team5/fud5/UserPreferencesActivity.java
+++ app/src/main/java/com/csc413/team5/fud5/UserPreferencesActivity.java
@@ -1,11 +1,11 @@
 package com.csc413.team5.fud5;
 
 import android.app.DialogFragment;
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 
 public class UserPreferencesActivity extends AppCompatActivity {
@@ -14,10 +14,99 @@ public class UserPreferencesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_preferences);
+
     }
 
-    public void OnEULAButtonClick(View v) {
-        DialogFragment eulaDialog = EulaDialogFragment.newInstance(false);
-        eulaDialog.show(getFragmentManager(), "EULA");
+    public void showFoodSettings(View v) {
+//        DialogFragment eulaDialog = EulaDialogFragment.newInstance(false);
+//        eulaDialog.show(getFragmentManager(), "EULA");
+
+        CharSequence text = "Showing Food Settings";
+
+        Toast toast = Toast.makeText(this, text, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void showAppSettings(View v) {
+//        DialogFragment eulaDialog = EulaDialogFragment.newInstance(false);
+//        eulaDialog.show(getFragmentManager(), "EULA");
+
+        CharSequence text = "Showing App Settings";
+        Toast toast = Toast.makeText(this, text, Toast.LENGTH_SHORT);
+
+        toast.show();
+    }
+
+    public void showEULA(View v) {
+//        DialogFragment eulaDialog = EulaDialogFragment.newInstance(false);
+//        eulaDialog.show(getFragmentManager(), "EULA");
+
+        CharSequence text = "Showing EULA";
+        Toast toast = Toast.makeText(this, text, Toast.LENGTH_SHORT);
+
+        toast.show();
+    }
+
+    public void showAppInfo(View v) {
+//        DialogFragment eulaDialog = EulaDialogFragment.newInstance(false);
+//        eulaDialog.show(getFragmentManager(), "EULA");
+
+        CharSequence text = "Showing app info";
+        Toast toast = Toast.makeText(this, text, Toast.LENGTH_SHORT);
+
+        toast.show();
+    }
+
+    public void showDbTest(View view){
+//        Intent intent = new Intent(this, UserPreferencesActivity.class);
+//        startActivity(intent);
+
+        CharSequence text = "Testing database";
+        Toast toast = Toast.makeText(this, text, Toast.LENGTH_SHORT);
+
+        toast.show();
+    }
+
+    public void showYelpTest(View view){
+//        Intent intent = new Intent(this, UserPreferencesActivity.class);
+//        startActivity(intent);
+
+        CharSequence text = "Testing Yelp integration";
+
+
+        Toast toast = Toast.makeText(this, text, Toast.LENGTH_SHORT);
+
+        toast.show();
+    }
+
+    public void showLocuTest(View view){
+//        Intent intent = new Intent(this, UserPreferencesActivity.class);
+//        startActivity(intent);
+
+        CharSequence text = "Testing Locu integration...";
+
+        Toast toast = Toast.makeText(this, text, Toast.LENGTH_SHORT);
+
+        toast.show();
+    }
+
+    public void showMapTest(View view){
+//        Intent intent = new Intent(this, UserPreferencesActivity.class);
+//        startActivity(intent);
+
+        CharSequence text = "Testing Google Maps integration...";
+        Toast toast = Toast.makeText(this, text, Toast.LENGTH_SHORT);
+
+        toast.show();
+    }
+
+    public void resetRedList(View view){
+//        Intent intent = new Intent(this, UserPreferencesActivity.class);
+//        startActivity(intent);
+
+        CharSequence text = "Successfully reset red list.";
+
+        Toast toast = Toast.makeText(this, text, Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
