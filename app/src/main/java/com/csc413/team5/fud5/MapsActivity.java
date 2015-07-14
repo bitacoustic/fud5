a5a9bee4f626b65c92f24564b7a6883fa97a1979
Ivan Gonzalez <Ivanglez43@gmail.com>
2015-07-14 14:07:19 -0700

    A map now displays on the results page. Also the layout was changed to look more like we want it to.

--- app/src/main/java/com/csc413/team5/fud5/MapsActivity.java
+++ app/src/main/java/com/csc413/team5/fud5/MapsActivity.java
@@ -60,6 +60,6 @@ public class MapsActivity extends FragmentActivity {
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(37.722039, -122.476640)).title("Marker"));
     }
 }
--- app/src/main/java/com/csc413/team5/fud5/ResultPageActivity.java
+++ app/src/main/java/com/csc413/team5/fud5/ResultPageActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 
 
 public class ResultPageActivity extends ActionBarActivity {
@@ -11,7 +12,9 @@ public class ResultPageActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //this.requestWindowFeature(activity_result_pag.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_result_page);
+
     }
 
 
