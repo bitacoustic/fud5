be0749665ae876e52694bb8f094fe0b1a722d2f7
awong9 <awong9@mail.sfsu.edu>
2015-07-19 11:07:28 -0700

    Fragments need a default constructor.

--- app/src/main/java/com/csc413/team5/fud5/ResultPageActivityFragment.java
+++ app/src/main/java/com/csc413/team5/fud5/ResultPageActivityFragment.java
@@ -12,8 +12,9 @@ import android.view.ViewGroup;
  */
 public class ResultPageActivityFragment extends Fragment {
 
+    public ResultPageActivityFragment() {
 
-
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
