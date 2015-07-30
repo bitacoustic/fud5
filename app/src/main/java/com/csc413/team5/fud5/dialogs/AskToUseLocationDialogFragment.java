720dbb72d4e279690c6807b7027566e0ada4687f
niculistana <niculistana@gmail.com>
2015-07-29 18:57:28 -0700

    Location Services Styling - Styled location services popup - Cleaned up EULA code

--- app/src/main/java/com/csc413/team5/fud5/dialogs/AskToUseLocationDialogFragment.java
+++ app/src/main/java/com/csc413/team5/fud5/dialogs/AskToUseLocationDialogFragment.java
@@ -1,42 +1,61 @@
 package com.csc413.team5.fud5.dialogs;
 
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
-import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
 
 import com.csc413.team5.fud5.R;
 
 public class AskToUseLocationDialogFragment extends DialogFragment {
     NoticeDialogListener mListener;
 
+    public static AskToUseLocationDialogFragment newInstance() {
+        return new AskToUseLocationDialogFragment();
+    }
+
+    /**
+     * @param savedInstanceState The last saved instance state of the Fragment,
+     *                           or null if this is a freshly created Fragment.
+     * @return Return a new Dialog instance to be displayed by the Fragment.
+     */
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setTitle(R.string.dialog_ask_to_use_location_title)
-                .setMessage(R.string.dialog_ask_to_use_location_body)
-                .setPositiveButton(R.string.dialog_ask_to_use_location_agree_button,
-                        new DialogInterface.OnClickListener() {
+        Dialog dialog = super.onCreateDialog(savedInstanceState);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        return dialog;
+    }
 
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                // Send the positive button event back to the host activity
-                                mListener.onLocationDialogPositiveClick(AskToUseLocationDialogFragment.this);
-                            }
-                        })
-                .setNegativeButton(R.string.dialog_ask_to_use_location_disagree_button,
-                        new DialogInterface.OnClickListener() {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_location_dialog, container, false);
 
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                // Send the negative button event back to the host activity
-                                mListener.onLocationDialogNegativeClick(AskToUseLocationDialogFragment.this);
-                            }
-                        });
-        //return super.onCreateDialog(savedInstanceState);
-        return builder.create();
+        Button posButton = (Button)v.findViewById(R.id.dialogLocationPositiveButton);
+        Button negButton = (Button)v.findViewById(R.id.dialogLocationNegativeButton);
+
+        // watch for button click
+        posButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mListener.onLocationDialogPositiveClick(AskToUseLocationDialogFragment.this);
+            }
+        });
+
+        // watch for button click
+        negButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mListener.onLocationDialogNegativeClick(AskToUseLocationDialogFragment.this);
+            }
+        });
+
+        return v;
     }
 
     public interface NoticeDialogListener {
